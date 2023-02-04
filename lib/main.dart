79a8f01a89573f90bf49c17b6f3e56619c5db5f7
Adesipe Adeola <princeadeola234@gmail.com>
2023-02-04 08:43:35 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("ExApp"),
+      centerTitle: false,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text(
+          "Hello Flutter World",
+          style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          fontFamily: 'PlayfairDisplay',
+        ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Text("Click"),
+      backgroundColor: Colors.red[600],
+    ),
+  ),
+));
