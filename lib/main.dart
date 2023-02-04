c730b55598c8e9538586972bb74fd525ae7d9d8e
Adesipe Adeola <princeadeola234@gmail.com>
2023-02-04 08:56:48 -0800

    use of Text()

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("ExApp"),
-      centerTitle: false,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
+  home: Home(),
+));
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ExApp"),
+        centerTitle: false,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
           "Hello Flutter World",
           style: TextStyle(
-          fontSize: 25.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          fontFamily: 'PlayfairDisplay',
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontFamily: 'PlayfairDisplay',
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text("Click"),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text("Click"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
